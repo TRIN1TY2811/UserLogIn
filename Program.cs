ac51318a9e77aa029d5a47a7f4b26ad0a8e11f11
Rianne Magtuba <jamessearll28@gmail.com>
2026-02-14 12:34:30 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Xml.Serialization;
+
+
+string username = "TRIN1TY";
+string password = "sigmaboi67";
+
+Console.WriteLine("Account Management System");
+Console.Write("Enter Username: ");
+string usernameInput = Console.ReadLine();
+Console.Write("Enter Password: ");
+string userPasswordInput = Console.ReadLine();
+
+if (usernameInput == username && userPasswordInput == password)
+{
+
+    Console.WriteLine("Login successful! Welcome, " + username + "!");
+
+
+
+}
+
+else
+{
+
+
+    Console.WriteLine("Incorrect username and password!");
+}
+
+
